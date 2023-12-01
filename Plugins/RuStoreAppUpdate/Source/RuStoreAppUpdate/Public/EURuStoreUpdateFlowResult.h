074997151646b86e7843e99ed2298a7ac232b8dd
rpelmegov <rpelmegov@ya.ru>
2023-12-01 07:01:36 +0300

    Update EURuStoreUpdateFlowResult.h

--- Plugins/RuStoreAppUpdate/Source/RuStoreAppUpdate/Public/EURuStoreUpdateFlowResult.h
+++ Plugins/RuStoreAppUpdate/Source/RuStoreAppUpdate/Public/EURuStoreUpdateFlowResult.h
@@ -9,5 +9,6 @@ UENUM(BlueprintType)
 enum class EURuStoreUpdateFlowResult : uint8
 {
     RESULT_OK UMETA(DisplayName = "RESULT_OK"),
-    RESULT_CANCELED UMETA(DisplayName = "RESULT_CANCELED")
+    RESULT_CANCELED UMETA(DisplayName = "RESULT_CANCELED"),
+    ACTIVITY_NOT_FOUND UMETA(DisplayName = "ACTIVITY_NOT_FOUND")
 };
